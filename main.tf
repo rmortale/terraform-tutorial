e144f778adbecd473c0eacfdf6cfadd2b1052e9e
edwin.torres@segurosbolivar.com-meteorologo <70587582+edwinrepo@users.noreply.github.com>
2023-08-11 06:49:25 -0500

    change in python version

--- main.tf
+++ main.tf
@@ -38,7 +38,7 @@ resource "aws_lambda_function" "lambda" {
 
   role    = "${aws_iam_role.iam_for_lambda.arn}"
   handler = "hello_lambda.lambda_handler"
-  runtime = "python3.6"
+  runtime = "python3.9"
 
   environment {
     variables = {
